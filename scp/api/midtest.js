68ad29281f748a63f5dfc75020afbdfa19bdcf9c
bmurdata <32182553+bmcap@users.noreply.github.com>
2019-08-01 17:55:03 -0400

    Further work on changing selected user using api, currently fails, auth form data not being sent to api.

--- scp/api/midtest.js
+++ scp/api/midtest.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 const personnel = require('../personnel');
 //attempt to use api to change logged in user- no success. 
-router.get('/:id',(req,res)=>{
-    console.log('accessing get for user');
-    const selecteduser = personnel.some(personnel => personnel.id === parseInt(req.params.id))
-user=personnel[selecteduser]
+router.get('/',(req,res)=>{
 
+console.log('Test of Get');
+res.send('test Passed');
+console.log(req.body);
+const selecteduser = personnel.some(personnel => personnel.id === parseInt(req.params.person));
+user=personnel[selecteduser];
+});
+//Quick Note- Only works with /api/midtest/Value, NOT api/midtest?person=3. Must fix.
+router.get('/:person',(req,res)=>{
+    console.log('accessing get for user');
+    const selecteduser = personnel.some(personnel => personnel.id === parseInt(req.params.person));
+user=selecteduser;
+console.log(user);
+res.send('Testing');
 });
 
 module.exports = router;
